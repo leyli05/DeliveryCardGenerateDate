d3115c07d5b322799206721108fc5d078ba134b2
leily <leily@example.com>
2024-02-25 12:34:38 +0300

    Added test and reformate code

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -2,12 +2,10 @@ package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
 import lombok.Value;
-import lombok.val;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
-import java.util.Random;
 
 public class DataGenerator {
     private DataGenerator() {
@@ -50,13 +48,11 @@ public class DataGenerator {
             return new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
         }
     }
+
     @Value
     public static class UserInfo {
         String city;
         String name;
         String phone;
-
-        public UserInfo(String city, String name, String phone) {
-        }
     }
 }
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -3,9 +3,14 @@ package ru.netology.delivery.test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
-import static com.codeborne.selenide.Selenide.open;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
 
@@ -23,6 +28,25 @@ class DeliveryTest {
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
         // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
+        $("[data-test-id=city] .input__control").setValue(validUser.getCity());
+        $("[data-test-id=date] .input__control").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
+        $("[data-test-id=date] .input__control").setValue(firstMeetingDate);
+        $("[data-test-id=name] .input__control").setValue(validUser.getName());
+        $("[data-test-id=phone] .input__control").setValue(validUser.getPhone());
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $$(".button").find(exactText("Запланировать")).click();
+        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(withText("Встреча успешно запланирована на")).shouldBe(visible, Duration.ofSeconds(15));
+        $(withText(firstMeetingDate)).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=success-notification] .icon-button__text").click();
+        $("[data-test-id=date] .input__control").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
+        $("[data-test-id=date] .input__control").setValue(secondMeetingDate);
+        $$(".button").find(exactText("Запланировать")).click();
+        $(withText("Необходимо подтверждение")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=replan-notification] .button__content").click();
+        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(withText("Встреча успешно запланирована на")).shouldBe(visible, Duration.ofSeconds(15));
+        $(withText(secondMeetingDate)).shouldBe(visible, Duration.ofSeconds(15));
         // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
         // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
         // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
