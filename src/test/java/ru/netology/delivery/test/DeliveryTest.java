52ed8266a78f9a52f9ec4c84fa02efdc70ceb3a6
leily <leily@example.com>
2024-02-27 16:34:14 +0300

    Made changes

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -36,8 +36,7 @@ class DeliveryTest {
         $("[data-test-id=agreement] .checkbox__box").click();
         $$(".button").find(exactText("Запланировать")).click();
         $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
-        $(withText("Встреча успешно запланирована на")).shouldBe(visible, Duration.ofSeconds(15));
-        $(withText(firstMeetingDate)).shouldBe(visible, Duration.ofSeconds(15));
+        $(withText("Встреча успешно запланирована на")).setValue(firstMeetingDate).shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id=success-notification] .icon-button__text").click();
         $("[data-test-id=date] .input__control").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
         $("[data-test-id=date] .input__control").setValue(secondMeetingDate);
@@ -45,11 +44,6 @@ class DeliveryTest {
         $(withText("Необходимо подтверждение")).shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id=replan-notification] .button__content").click();
         $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
-        $(withText("Встреча успешно запланирована на")).shouldBe(visible, Duration.ofSeconds(15));
-        $(withText(secondMeetingDate)).shouldBe(visible, Duration.ofSeconds(15));
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
+        $(withText("Встреча успешно запланирована на")).setValue(secondMeetingDate).shouldBe(visible, Duration.ofSeconds(15));
     }
 }
